edbdb4001821b7f653d5d1cbf0cf0c5d8e292a34
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-23 19:17:30 +0200

    Basic app structure moved to src/app.js. Added basic template

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const port = process.env.PORT || 4000
+
+// Define path for Express configuration
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// setup static directory to use
+app.use(express.static(publicDirPath))
+
+
+// home page
+app.get('/', (req, res) => {
+    // geolocalizzati, se fallisce usa impostazioni predefinite
+
+    // ottieni meteo
+
+    // ottieni calendario
+
+    // render homepage
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log('Server is listening on port ', port)
+})
