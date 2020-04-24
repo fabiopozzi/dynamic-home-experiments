6e144d0dc285fd1ffa30bb79dacccd787e981047
Fabio Pozzi <fabio@fabiopozzi.net>
2020-04-24 10:54:09 +0200

    Add dotenv to store env variables into .env file (not versioned)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 4000
 
-// Define path for Express configuration
+// Define paths for Express configuration
 const publicDirPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
